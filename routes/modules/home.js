25e7b6d55c29b537f3b22e4b1409ef716fdb7ac8
Flipapa <roamer2010x@gmail.com>
2021-07-21 15:51:24 +0800

    feat: add home page data

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,8 +1,20 @@
 const express = require('express')
 const router = express.Router()
+const Record = require('../../models/record')
+const Category = require('../../models/category')
 
+const categoryList = []
+Category.find()
+  .lean()
+  .then(categories => 
+    categories.forEach(category => categoryList.push(category.name))
+  )
 router.get('/', (req, res) => {
-  res.render('index')
+  Record.find()
+    .lean()
+    .sort({date: 'desc'})
+    .then(records => res.render('index', { records, categoryList }))
+    .catch(error => console.error(error))
 })
 
 module.exports = router
